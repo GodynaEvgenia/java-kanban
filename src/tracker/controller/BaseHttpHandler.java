edfa08ec0b0efe3b7df5a4775a29e6612e60d554
Evgeniia Godyna <evgeniagodyna@yandex.ru>
2025-04-28 23:38:37 +0300

    Финальное задание спринт 9 кодировка

--- src/tracker/controller/BaseHttpHandler.java
+++ src/tracker/controller/BaseHttpHandler.java
@@ -10,7 +10,7 @@ public class BaseHttpHandler {
     private static final Charset DEFAULT_CHARSET = StandardCharsets.UTF_8;
 
     protected static void sendText(HttpExchange h, String text) throws IOException {
-        byte[] resp = text.getBytes(StandardCharsets.UTF_8);
+        byte[] resp = text.getBytes(DEFAULT_CHARSET);
         h.getResponseHeaders().add("Content-Type", "application/json;charset=utf-8");
         h.sendResponseHeaders(200, resp.length);
         h.getResponseBody().write(resp);
@@ -18,7 +18,7 @@ public class BaseHttpHandler {
     }
 
     protected static void sendNotFound(HttpExchange h, String text) throws IOException {
-        byte[] resp = text.getBytes(StandardCharsets.UTF_8);
+        byte[] resp = text.getBytes(DEFAULT_CHARSET);
         h.getResponseHeaders().add("Content-Type", "application/json;charset=utf-8");
         h.sendResponseHeaders(404, resp.length);
         h.getResponseBody().write(resp);
@@ -26,18 +26,10 @@ public class BaseHttpHandler {
     }
 
     protected void sendHasInterraction(HttpExchange h, String text) throws IOException {
-        byte[] resp = text.getBytes(StandardCharsets.UTF_8);
+        byte[] resp = text.getBytes(DEFAULT_CHARSET);
         h.getResponseHeaders().add("Content-Type", "application/json;charset=utf-8");
         h.sendResponseHeaders(406, resp.length);
         h.getResponseBody().write(resp);
         h.close();
-    }
-
-    protected void sendInternalError(HttpExchange h, String text) {
-        /*try (OutputStream os = h.getResponseBody()) {
-            h.sendResponseHeaders(500, 0);
-            os.write("Внутренняя ошибка".getBytes(DEFAULT_CHARSET));
-        }
-        h.close();*/
     }
 }
